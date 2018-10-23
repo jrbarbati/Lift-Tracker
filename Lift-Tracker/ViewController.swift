32dd796686ab300126dd784df39e84c482d3bc77
jxb4933 <jxb4933@homedepot.com>
2018-10-23 19:05:21 -0400

    initial commit

---
+++ Lift-Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lift-Tracker
+//
+//  Created by Joseph Barbati on 10/23/18.
+//  Copyright © 2018 Joseph Barbati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
